d8331c281621b0c6a83f2abd8dfcf70102c482f8
Benjamin_duclos <benjaminduclos@dbmail.com>
2014-02-09 14:39:27 +0100

    Redirection de la page POST

--- minichat/minichat_post.php
+++ minichat/minichat_post.php
@@ -1,4 +1,30 @@
 
+
+
+<?php
+// Effectuer ici la requête qui insère le message
+// Puis rediriger vers minichat.php comme ceci :
+header('Location: minichat.php');
+?>
+
+
+
+
+<?php
+try
+{
+    $bdd = new PDO('mysql:host=localhost;dbname=test', 'root', 'LEloBA42');
+}
+catch (Exception $e)
+{
+        die('Erreur : ' . $e->getMessage());
+}
+?>
+
+
+header('Location:minichat.php')
+
+
 <head>
 <h2> Post du message</h2>
 <?php echo $_POST['pseudo'];?></br><?php echo $_POST['message'];?>
